90d4acd80176f8f378fe7bb1b89dc2a9bd75cb9a
nischalrajc <nischalraj22@gmail.com>
2023-07-01 17:23:16 +0530

    updated

--- helpers/userhelpers.js
+++ helpers/userhelpers.js
@@ -476,56 +476,7 @@ module.exports = {
       }
     },
 
-
-    //------------PayPal-------------//
-
-    // async generatePayPal(orderId,total){
-    //   console.log("deep iside")
-    //   try{
-    //     const create_payment_json = {
-    //       "intent": "sale",
-    //       "payer": {
-    //           "payment_method": "paypal"
-    //       },
-    //       "redirect_urls": {
-    //           "return_url": "http://localhost:3000/success",
-    //           "cancel_url": "http://localhost:3000/cancel"
-    //       },
-    //       "transactions": [{
-    //           "item_list": {
-    //               "items": [{
-    //                   "name": "item",
-    //                   "sku": "item",
-    //                   "price": total,
-    //                   "currency": "USD",
-    //                   "quantity": 1
-    //               }]
-    //           },
-    //           "amount": {
-    //               "currency": "USD",
-    //               "total": total
-    //           },
-    //           "description": "This is the payment description."
-    //       }]
-    //   };
-
-       
-    //     paypal.payment.create(create_payment_json, function (error, payment) {
-    //       if (error) {
-    //           throw error;
-    //       } else {
-    //           console.log("Create Payment Response");
-    //           console.log(payment,"gggggggggg");
-    //           return  payment
-    //       }
-    //     });
-        
-    //   }catch(err){
-    //     console.log("error:",err)
-    //   }
-    // }
-
-   
+   //---------PayPal--------------
 
 
     async  generatePayPal(orderId, total) {
